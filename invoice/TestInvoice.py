4f4628b400794f2e4870395b796f5b978fabbdc5
Chris Jeffcoat <38119321+cjeffcoatjr@users.noreply.github.com>
2020-03-18 16:39:34 -0400

    Intial upload of original repository.
    The original repository can be found at https://github.com/cjeffcoatjr/invoice.

---
+++ invoice/TestInvoice.py
@@ -0,0 +1,34 @@
+import pytest
+from Invoice import Invoice
+
+
+@pytest.fixture()
+def products():
+    products = {"Pen": {'qnt': 10, 'unit_price': 3.75, 'discount': 5},
+                'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
+    return products
+
+
+@pytest.fixture()
+def invoice():
+    invoice = Invoice()
+    return invoice
+
+
+def test_CanCalculateTotalImpurePrice(invoice, products):
+    testTotalImpurePrice = invoice.totalImpurePrice(products)
+    assert testTotalImpurePrice == 75
+
+
+def test_CanCalculateTotalDiscount(invoice, products):
+    testTotalDiscount = invoice.totalDiscount(products)
+    assert testTotalDiscount == 5.62
+
+
+def test_CanCalculateTotalPurePrice(invoice, products):
+    testTotalPurePrice = invoice.totalPurePrice(products)
+    assert testTotalPurePrice == 69.38
+
+def test_CanInputAnswer(invoice):
+    testInputAnswer = invoice.inputAnswer("Pen")
+    assert testInputAnswer == "Pen"
