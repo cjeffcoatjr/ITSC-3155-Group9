1bc92f9f9a98c46d79296b299489c29eecf1c1c4
cjeffcoatjr <cjeffcoatjr@gmail.com>
2020-04-14 19:53:49 -0400

    Adding changes to the dashboard.

---
+++ python-dash/Dashboards/interactivedashboard.py
@@ -0,0 +1,137 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.graph_objs as go
+
+# Load CSV file from Datasets folder
+df1 = pd.read_csv('../Datasets/Olympic2016Rio.csv')
+df2 = pd.read_csv('../Datasets/Weather2014-15.csv')
+
+app = dash.Dash()
+
+# Bar chart data
+barchart_df1 = df1
+barchart_df1 = barchart_df1.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+barchart_df1 = barchart_df1.groupby(['NOC'])['Total'].sum().reset_index()
+barchart_df1 = barchart_df1.sort_values(by=['Total'], ascending=[False]).head(20)
+data_barchart = [go.Bar(x=barchart_df1['NOC'], y=barchart_df1['Total'])]
+
+# Line Chart
+line_df2 = df2
+line_df2['date'] = pd.to_datetime(line_df2['date'])
+data_linechart = [go.Scatter(x=line_df2['date'], y=line_df2['actual_mean_temp'], mode='lines', name='mean Temps')]
+
+# Multi Line Chart
+multiline_df2 = df2
+multiline_df2['date'] = pd.to_datetime(multiline_df2['date'])
+trace1_multiline = go.Scatter(x=multiline_df2['date'], y=multiline_df2['actual_min_temp'], mode='lines', name='min')
+trace2_multiline = go.Scatter(x=multiline_df2['date'], y=multiline_df2['actual_max_temp'], mode='lines', name='max')
+trace3_multiline = go.Scatter(x=multiline_df2['date'], y=multiline_df2['actual_mean_temp'], mode='lines',
+                              name='mean')
+data_multiline = [trace1_multiline, trace2_multiline, trace3_multiline]
+
+# Bubble chart
+# bubble_df2 = df2
+# bubble_df2 = bubble_df2.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+# bubble_df2['Unrecovered'] = bubble_df2['Confirmed'] - bubble_df2['Deaths'] - bubble_df2['Recovered']
+# data_bubblechart = [
+#      go.Scatter(x=bubble_df2['date'],
+#                 y=bubble_df2['average_Precipitation'],
+#                 text=bubble_df2['day'],
+#                 mode='markers',
+#                 marker=dict(size=bubble_df2['average_Precipitation'] / 200, color=bubble_df2['average_Precipitation'] / 200, showscale=True))
+#  ]
+
+# Heatmap
+data_heatmap = [go.Heatmap(x=df2['day'],
+                           y=df2['month'],
+                           z=df2['actual_mean_temp'].values.tolist(),
+                           colorscale='Jet')]
+
+# Layout
+app.layout = html.Div(children=[
+    html.H1(children='Python Dash',
+            style={
+                'textAlign': 'center',
+                'color': '#ef3e18'
+            }
+            ),
+    html.Div('Web dashboard for Data Visualization using Python', style={'textAlign': 'center'}),
+    html.Div('Aggregated data from the Rio 2016 Olympics and Weather Data from 2014 and 2015.', style={'textAlign': 'center'}),
+    html.Br(),
+    html.Br(),
+    html.Hr(style={'color': '#7FDBFF'}),
+    html.H3('Bar Chart', style={'color': '#df2e56'}),
+    html.Div('This bar chart represents the number of total olympic medals earned by the top 20 countries.'),
+    dcc.Graph(id='graph1',
+              figure={
+                  'data': data_barchart,
+                  'layout': go.Layout(title='Top 20 countries, sorted by total medal count. ',
+                                      xaxis={'title': 'Countries'}, yaxis={'title': 'Number of total medals'})
+              }
+              ),
+    html.Hr(style={'color': '#7FDBFF'}),
+    html.H3('Line chart', style={'color': '#df2e56'}),
+    html.Div('This line chart represent the Corona Virus confirmed cases of all reported cases in the given period.'),
+    dcc.Graph(id='graph4',
+           figure={
+               'data': data_linechart,
+               'layout': go.Layout(title='Corona Virus Confirmed Cases From 2020-01-22 to 2020-03-17',
+                                       xaxis={'title': 'Date'}, yaxis={'title': 'Number of cases'})
+               }
+               ),
+    html.Hr(style={'color': '#7FDBFF'}),
+    html.H3('Multi Line chart', style={'color': '#df2e56'}),
+    html.Div(
+        'This line chart represent the CoronaVirus death, recovered and under treatment cases of all reported cases in the given period.'),
+    dcc.Graph(id='graph5',
+              figure={
+                  'data': data_multiline,
+                  'layout': go.Layout(
+                      title='Corona Virus Death, Recovered and under treatment Cases From 2020-01-22 to 2020-03-17',
+                      xaxis={'title': 'Date'}, yaxis={'title': 'Number of cases'})
+              }
+              ),
+    # html.Hr(style={'color': '#7FDBFF'}),
+    # html.H3('Bubble chart', style={'color': '#df2e56'}),
+    # html.Div(
+    #     'This bubble chart represent the Corona Virus recovered and under treatment of all reported countries except China.'),
+    # dcc.Graph(id='graph6',
+    #           figure={
+    #               'data': data_bubblechart,
+    #               'layout': go.Layout(title='Corona Virus Confirmed Cases',
+    #                                   xaxis={'title': 'Recovered Cases'}, yaxis={'title': 'under Treatment Cases'},
+    #                                   hovermode='closest')
+    #           }
+    #           ),
+    html.Hr(style={'color': '#7FDBFF'}),
+    html.H3('Heat map', style={'color': '#df2e56'}),
+    html.Div(
+        'This heat map represent the Corona Virus recovered cases of all reported cases per day of week and week of month.'),
+    dcc.Graph(id='graph7',
+              figure={
+                  'data': data_heatmap,
+                  'layout': go.Layout(title='Corona Virus Recovered Cases',
+                                      xaxis={'title': 'Day of Week'}, yaxis={'title': 'Week of Month'})
+              }
+              )
+])
+
+# @app.callback(Output('graph1', 'figure'),
+#               [Input('select-continent', 'value')])
+# def update_figure(selected_continent):
+#     filtered_df2 = df2[df2['Continent'] == selected_continent]
+#
+#     filtered_df2 = filtered_df2.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+#     new_df2 = filtered_df2.groupby(['Country'])['Confirmed'].sum().reset_index()
+#     new_df2 = new_df2.sort_values(by=['Confirmed'], ascending=[False]).head(20)
+#     data_interactive_barchart = [go.Bar(x=new_df2['Country'], y=new_df2['Confirmed'])]
+#     return {'data': data_interactive_barchart,
+#             'layout': go.Layout(title='Corona Virus Confirmed Cases in ' + selected_continent,
+#                                 xaxis={'title': 'Country'},
+#                                 yaxis={'title': 'Number of confirmed cases'})}
+
+if __name__ == '__main__':
+    app.run_server()
--- python-dash/Dashboards/interactivedashboardrio.py
+++ python-dash/Dashboards/interactivedashboardrio.py
@@ -6,66 +6,66 @@ import pandas as pd
 import plotly.graph_objs as go
 
 # Load CSV file from Datasets folder
-df = pd.read_csv('../Datasets/Olympic2016Rio.csv')
+df1 = pd.read_csv('../Datasets/Olympic2016Rio.csv')
 
 app = dash.Dash()
 
 # Bar chart data
-barchart_df = df[df['Country'] == 'US']
-barchart_df = barchart_df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-barchart_df = barchart_df.groupby(['State'])['Confirmed'].sum().reset_index()
-barchart_df = barchart_df.sort_values(by=['Confirmed'], ascending=[False]).head(20)
-data_barchart = [go.Bar(x=barchart_df['State'], y=barchart_df['Confirmed'])]
+barchart_df1 = df1
+barchart_df1 = barchart_df1.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+barchart_df1 = barchart_df1.groupby(['NOC'])['Total'].sum().reset_index()
+barchart_df1 = barchart_df1.sort_values(by=['Total'], ascending=[False]).head(20)
+data_barchart = [go.Bar(x=barchart_df1['NOC'], y=barchart_df1['Total'])]
 
-# Stack bar chart data
-stackbarchart_df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-stackbarchart_df['Unrecovered'] = stackbarchart_df['Confirmed'] - stackbarchart_df['Deaths'] - stackbarchart_df[
-    'Recovered']
-stackbarchart_df = stackbarchart_df[(stackbarchart_df['Country'] != 'China')]
-stackbarchart_df = stackbarchart_df.groupby(['Country']).agg(
-    {'Confirmed': 'sum', 'Deaths': 'sum', 'Recovered': 'sum', 'Unrecovered': 'sum'}).reset_index()
-stackbarchart_df = stackbarchart_df.sort_values(by=['Confirmed'], ascending=[False]).head(20).reset_index()
-trace1_stackbarchart = go.Bar(x=stackbarchart_df['Country'], y=stackbarchart_df['Unrecovered'], name='Under Treatment',
-                              marker={'color': '#CD7F32'})
-trace2_stackbarchart = go.Bar(x=stackbarchart_df['Country'], y=stackbarchart_df['Recovered'], name='Recovered',
-                              marker={'color': '#9EA0A1'})
-trace3_stackbarchart = go.Bar(x=stackbarchart_df['Country'], y=stackbarchart_df['Deaths'], name='Deaths',
-                              marker={'color': '#FFD700'})
-data_stackbarchart = [trace1_stackbarchart, trace2_stackbarchart, trace3_stackbarchart]
+# # Stack bar chart data
+# stackbarchart_df1 = df1.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+# stackbarchart_df1['Unrecovered'] = stackbarchart_df1['Confirmed'] - stackbarchart_df1['Deaths'] - stackbarchart_df1[
+#     'Recovered']
+# stackbarchart_df1 = stackbarchart_df1[(stackbarchart_df1['Country'] != 'China')]
+# stackbarchart_df1 = stackbarchart_df1.groupby(['Country']).agg(
+#     {'Confirmed': 'sum', 'Deaths': 'sum', 'Recovered': 'sum', 'Unrecovered': 'sum'}).reset_index()
+# stackbarchart_df1 = stackbarchart_df1.sort_values(by=['Confirmed'], ascending=[False]).head(20).reset_index()
+# trace1_stackbarchart = go.Bar(x=stackbarchart_df1['Country'], y=stackbarchart_df1['Unrecovered'], name='Under Treatment',
+#                               marker={'color': '#CD7F32'})
+# trace2_stackbarchart = go.Bar(x=stackbarchart_df1['Country'], y=stackbarchart_df1['Recovered'], name='Recovered',
+#                               marker={'color': '#9EA0A1'})
+# trace3_stackbarchart = go.Bar(x=stackbarchart_df1['Country'], y=stackbarchart_df1['Deaths'], name='Deaths',
+#                               marker={'color': '#FFD700'})
+# data_stackbarchart = [trace1_stackbarchart, trace2_stackbarchart, trace3_stackbarchart]
 
-# Line Chart
-line_df = df
-line_df['Date'] = pd.to_datetime(line_df['Date'])
-data_linechart = [go.Scatter(x=line_df['Date'], y=line_df['Confirmed'], mode='lines', name='Death')]
+# # Line Chart
+# line_df1 = df1
+# line_df1['Date'] = pd.to_datetime(line_df1['Date'])
+# data_linechart = [go.Scatter(x=line_df1['Date'], y=line_df1['Confirmed'], mode='lines', name='Death')]
 
-# Multi Line Chart
-multiline_df = df
-multiline_df['Date'] = pd.to_datetime(multiline_df['Date'])
-trace1_multiline = go.Scatter(x=multiline_df['Date'], y=multiline_df['Death'], mode='lines', name='Death')
-trace2_multiline = go.Scatter(x=multiline_df['Date'], y=multiline_df['Recovered'], mode='lines', name='Recovered')
-trace3_multiline = go.Scatter(x=multiline_df['Date'], y=multiline_df['Unrecovered'], mode='lines',
-                              name='Under Treatment')
-data_multiline = [trace1_multiline, trace2_multiline, trace3_multiline]
+# # Multi Line Chart
+# multiline_df1 = df1
+# multiline_df1['Date'] = pd.to_datetime(multiline_df1['Date'])
+# trace1_multiline = go.Scatter(x=multiline_df1['Date'], y=multiline_df1['Death'], mode='lines', name='Death')
+# trace2_multiline = go.Scatter(x=multiline_df1['Date'], y=multiline_df1['Recovered'], mode='lines', name='Recovered')
+# trace3_multiline = go.Scatter(x=multiline_df1['Date'], y=multiline_df1['Unrecovered'], mode='lines',
+#                               name='Under Treatment')
+# data_multiline = [trace1_multiline, trace2_multiline, trace3_multiline]
 
 # Bubble chart
-bubble_df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-bubble_df['Unrecovered'] = bubble_df['Confirmed'] - bubble_df['Deaths'] - bubble_df['Recovered']
-bubble_df = bubble_df[(bubble_df['Country'] != 'China')]
-bubble_df = bubble_df.groupby(['Country']).agg(
-    {'Confirmed': 'sum', 'Recovered': 'sum', 'Unrecovered': 'sum'}).reset_index()
-data_bubblechart = [
-    go.Scatter(x=bubble_df['Recovered'],
-               y=bubble_df['Unrecovered'],
-               text=bubble_df['Country'],
-               mode='markers',
-               marker=dict(size=bubble_df['Confirmed'] / 200, color=bubble_df['Confirmed'] / 200, showscale=True))
-]
+# bubble_df1 = df1.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+# bubble_df1['Unrecovered'] = bubble_df1['Confirmed'] - bubble_df1['Deaths'] - bubble_df1['Recovered']
+# bubble_df1 = bubble_df1[(bubble_df1['Country'] != 'China')]
+# bubble_df1 = bubble_df1.groupby(['Country']).agg(
+#     {'Confirmed': 'sum', 'Recovered': 'sum', 'Unrecovered': 'sum'}).reset_index()
+# data_bubblechart = [
+#     go.Scatter(x=bubble_df1['Recovered'],
+#                y=bubble_df1['Unrecovered'],
+#                text=bubble_df1['Country'],
+#                mode='markers',
+#                marker=dict(size=bubble_df1['Confirmed'] / 200, color=bubble_df1['Confirmed'] / 200, showscale=True))
+# ]
 
 # Heatmap
-data_heatmap = [go.Heatmap(x=df['Day'],
-                           y=df['WeekofMonth'],
-                           z=df['Recovered'].values.tolist(),
-                           colorscale='Jet')]
+# data_heatmap = [go.Heatmap(x=df1['Day'],
+#                            y=df1['WeekofMonth'],
+#                            z=df1['Recovered'].values.tolist(),
+#                            colorscale='Jet')]
 
 # Layout
 app.layout = html.Div(children=[
@@ -80,7 +80,7 @@ app.layout = html.Div(children=[
     html.Br(),
     html.Br(),
     html.Hr(style={'color': '#7FDBFF'}),
-    html.H3('Interactive Bar chart', style={'color': '#dfe56'}),
+    html.H3('Interactive Bar chart', style={'color': '#df1e56'}),
     html.Div('This bar chart represent the number of confirmed cases in the first 20 countries of selected continent.'),
     dcc.Graph(id='graph1'),
     html.Div('Please select a continent', style={'color': '#ef3e18', 'margin': '10px'}),
@@ -98,7 +98,7 @@ app.layout = html.Div(children=[
     ),
     html.Br(),
     html.Hr(style={'color': '#7FDBFF'}),
-    html.H3('Bar chart', style={'color': '#dfe56'}),
+    html.H3('Bar chart', style={'color': '#df1e56'}),
     html.Div('This bar chart represent the number of confirmed cases in the first 20 states of the US.'),
     dcc.Graph(id='graph2',
               figure={
@@ -108,74 +108,74 @@ app.layout = html.Div(children=[
               }
               ),
     html.Hr(style={'color': '#7FDBFF'}),
-    html.H3('Stack bar chart', style={'color': '#dfe56'}),
+    html.H3('Stack bar chart', style={'color': '#df1e56'}),
     html.Div(
         'This stack bar chart represent the CoronaVirus deaths, recovered and under treatment of all reported first 20 countries except China.'),
-    dcc.Graph(id='graph3',
-              figure={
-                  'data': data_stackbarchart,
-                  'layout': go.Layout(title='Corona Virus Cases in the first 20 country expect China',
-                                      xaxis={'title': 'Country'}, yaxis={'title': 'Number of cases'},
-                                      barmode='stack')
-              }
-              ),
+    # dcc.Graph(id='graph3',
+    #           figure={
+    #               'data': data_stackbarchart,
+    #               'layout': go.Layout(title='Corona Virus Cases in the first 20 country expect China',
+    #                                   xaxis={'title': 'Country'}, yaxis={'title': 'Number of cases'},
+    #                                   barmode='stack')
+    #           }
+    #           ),
     html.Hr(style={'color': '#7FDBFF'}),
-    html.H3('Line chart', style={'color': '#dfe56'}),
+    html.H3('Line chart', style={'color': '#df1e56'}),
     html.Div('This line chart represent the Corona Virus confirmed cases of all reported cases in the given period.'),
-    dcc.Graph(id='graph4',
-              figure={
-                  'data': data_linechart,
-                  'layout': go.Layout(title='Corona Virus Confirmed Cases From 2020-01-22 to 2020-03-17',
-                                      xaxis={'title': 'Date'}, yaxis={'title': 'Number of cases'})
-              }
-              ),
+    # dcc.Graph(id='graph4',
+    #           figure={
+    #               'data': data_linechart,
+    #               'layout': go.Layout(title='Corona Virus Confirmed Cases From 2020-01-22 to 2020-03-17',
+    #                                   xaxis={'title': 'Date'}, yaxis={'title': 'Number of cases'})
+    #           }
+    #           ),
     html.Hr(style={'color': '#7FDBFF'}),
-    html.H3('Multi Line chart', style={'color': '#dfe56'}),
+    html.H3('Multi Line chart', style={'color': '#df1e56'}),
     html.Div(
         'This line chart represent the CoronaVirus death, recovered and under treatment cases of all reported cases in the given period.'),
-    dcc.Graph(id='graph5',
-              figure={
-                  'data': data_multiline,
-                  'layout': go.Layout(
-                      title='Corona Virus Death, Recovered and under treatment Cases From 2020-01-22 to 2020-03-17',
-                      xaxis={'title': 'Date'}, yaxis={'title': 'Number of cases'})
-              }
-              ),
+    # dcc.Graph(id='graph5',
+    #           figure={
+    #               'data': data_multiline,
+    #               'layout': go.Layout(
+    #                   title='Corona Virus Death, Recovered and under treatment Cases From 2020-01-22 to 2020-03-17',
+    #                   xaxis={'title': 'Date'}, yaxis={'title': 'Number of cases'})
+    #           }
+    #           ),
     html.Hr(style={'color': '#7FDBFF'}),
-    html.H3('Bubble chart', style={'color': '#dfe56'}),
+    html.H3('Bubble chart', style={'color': '#df1e56'}),
     html.Div(
         'This bubble chart represent the Corona Virus recovered and under treatment of all reported countries except China.'),
-    dcc.Graph(id='graph6',
-              figure={
-                  'data': data_bubblechart,
-                  'layout': go.Layout(title='Corona Virus Confirmed Cases',
-                                      xaxis={'title': 'Recovered Cases'}, yaxis={'title': 'under Treatment Cases'},
-                                      hovermode='closest')
-              }
-              ),
+    # dcc.Graph(id='graph6',
+    #           figure={
+    #               'data': data_bubblechart,
+    #               'layout': go.Layout(title='Corona Virus Confirmed Cases',
+    #                                   xaxis={'title': 'Recovered Cases'}, yaxis={'title': 'under Treatment Cases'},
+    #                                   hovermode='closest')
+    #           }
+    #           ),
     html.Hr(style={'color': '#7FDBFF'}),
-    html.H3('Heat map', style={'color': '#dfe56'}),
+    html.H3('Heat map', style={'color': '#df1e56'}),
     html.Div(
         'This heat map represent the Corona Virus recovered cases of all reported cases per day of week and week of month.'),
-    dcc.Graph(id='graph7',
-              figure={
-                  'data': data_heatmap,
-                  'layout': go.Layout(title='Corona Virus Recovered Cases',
-                                      xaxis={'title': 'Day of Week'}, yaxis={'title': 'Week of Month'})
-              }
-              )
+    # dcc.Graph(id='graph7',
+    #           figure={
+    #               'data': data_heatmap,
+    #               'layout': go.Layout(title='Corona Virus Recovered Cases',
+    #                                   xaxis={'title': 'Day of Week'}, yaxis={'title': 'Week of Month'})
+    #           }
+    #           )
 ])
 
 
 @app.callback(Output('graph1', 'figure'),
               [Input('select-continent', 'value')])
 def update_figure(selected_continent):
-    filtered_df = df[df['Continent'] == selected_continent]
+    filtered_df1 = df1[df1['Continent'] == selected_continent]
 
-    filtered_df = filtered_df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-    new_df = filtered_df.groupby(['Country'])['Confirmed'].sum().reset_index()
-    new_df = new_df.sort_values(by=['Confirmed'], ascending=[False]).head(20)
-    data_interactive_barchart = [go.Bar(x=new_df['Country'], y=new_df['Confirmed'])]
+    filtered_df1 = filtered_df1.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    new_df1 = filtered_df1.groupby(['Country'])['Confirmed'].sum().reset_index()
+    new_df1 = new_df1.sort_values(by=['Confirmed'], ascending=[False]).head(20)
+    data_interactive_barchart = [go.Bar(x=new_df1['Country'], y=new_df1['Confirmed'])]
     return {'data': data_interactive_barchart,
             'layout': go.Layout(title='Corona Virus Confirmed Cases in ' + selected_continent,
                                 xaxis={'title': 'Country'},
