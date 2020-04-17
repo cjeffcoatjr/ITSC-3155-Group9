fc0e41f500ade846b8ef60fd5047a2803571fc37
cjeffcoatjr <cjeffcoatr@gmail.com>
2020-04-17 18:25:24 -0400

    JSON Example

--- final-project/Data/ChoroplethEx.py
+++ final-project/Data/ChoroplethEx.py
@@ -1,9 +1,16 @@
 import plotly.graph_objects as go
+import requests
+import pandas as pd # Load data frame and tidy it.
 
-# Load data frame and tidy it.
-import pandas as pd
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
-# df = pd.read_json('GOES HERE')
+response = requests.get("https://corona.lmao.ninja/v2/states")
+if response.status_code == 200:
+    stateDict = response.json()
+else:
+    print("error, server responded with status code of" + str(response.status_code))
+    exit(-1)
+
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+df = pd.read_json(stateDict)
 fig = go.Figure(data=go.Choropleth(
     locations=df['code'], # Spatial coordinates
     z = df['total exports'].astype(float), # Data to be color-coded
