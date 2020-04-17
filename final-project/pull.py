0f2512b616ebbabe1d22c2dec6398d80c6f2b7a9
David <dstokes5@uncc.edu>
2020-04-17 17:38:57 -0400

    Data Pull Basic Prototype

---
+++ final-project/pull.py
@@ -0,0 +1,8 @@
+import requests
+
+
+response = requests.get("https://corona.lmao.ninja/v2/states")
+if response.status_code == 200:
+    stateDict = response.json()
+else:
+    print("error, server responded with status code of" + str(response.status_code))
