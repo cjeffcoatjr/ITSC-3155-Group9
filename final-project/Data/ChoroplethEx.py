01e4510997b83f155baceb53652c1549aca5e749
cjeffcoatjr <cjeffcoatr@gmail.com>
2020-04-17 19:54:50 -0400

    Seperated States.py

--- final-project/Data/ChoroplethEx.py
+++ final-project/Data/ChoroplethEx.py
@@ -1,7 +1,7 @@
 import plotly.graph_objects as go
 import requests
-import pandas as pd # Load data frame and tidy it.
-
+import pandas as pd  # Load data frame and tidy it.
+from Data import States as states
 
 response = requests.get("https://corona.lmao.ninja/v2/states")
 if response.status_code == 200:
@@ -11,70 +11,11 @@ if response.status_code == 200:
 else:
     print("error, server responded with status code of" + str(response.status_code))
     exit(-1)
-#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
 df = pd.read_json(stateDict)
 
+states = states.states
 
-
-states = {
-        'AK': 'Alaska',
-        'AL': 'Alabama',
-        'AR': 'Arkansas',
-        'AS': 'American Samoa',
-        'AZ': 'Arizona',
-        'CA': 'California',
-        'CO': 'Colorado',
-        'CT': 'Connecticut',
-        'DC': 'District of Columbia',
-        'DE': 'Delaware',
-        'FL': 'Florida',
-        'GA': 'Georgia',
-        'GU': 'Guam',
-        'HI': 'Hawaii',
-        'IA': 'Iowa',
-        'ID': 'Idaho',
-        'IL': 'Illinois',
-        'IN': 'Indiana',
-        'KS': 'Kansas',
-        'KY': 'Kentucky',
-        'LA': 'Louisiana',
-        'MA': 'Massachusetts',
-        'MD': 'Maryland',
-        'ME': 'Maine',
-        'MI': 'Michigan',
-        'MN': 'Minnesota',
-        'MO': 'Missouri',
-        'MP': 'Northern Mariana Islands',
-        'MS': 'Mississippi',
-        'MT': 'Montana',
-        'NA': 'National',
-        'NC': 'North Carolina',
-        'ND': 'North Dakota',
-        'NE': 'Nebraska',
-        'NH': 'New Hampshire',
-        'NJ': 'New Jersey',
-        'NM': 'New Mexico',
-        'NV': 'Nevada',
-        'NY': 'New York',
-        'OH': 'Ohio',
-        'OK': 'Oklahoma',
-        'OR': 'Oregon',
-        'PA': 'Pennsylvania',
-        'PR': 'Puerto Rico',
-        'RI': 'Rhode Island',
-        'SC': 'South Carolina',
-        'SD': 'South Dakota',
-        'TN': 'Tennessee',
-        'TX': 'Texas',
-        'UT': 'Utah',
-        'VA': 'Virginia',
-        'VI': 'Virgin Islands',
-        'VT': 'Vermont',
-        'WA': 'Washington',
-        'WI': 'Wisconsin',
-        'WV': 'West Virginia',
-        'WY': 'Wyoming' # <---- YIKES!!!!
-}
 for col in df.columns:
     df[col] = df[col].astype(str)
 
@@ -89,22 +30,24 @@ fig = go.Figure(data=go.Choropleth(
     locationmode='USA-states',
     colorscale='Reds',
     autocolorscale=False,
-    text=df['text'], # hover text
-    marker_line_color='white', # line markers between states
+    text=df['text'],  # hover text
+    marker_line_color='white',  # line markers between states
     colorbar_title="Infected",
     geoJSON=True,
 ))
 
-
 fig.update_layout(
-    #title_text='Covid-19 In the United States and Wyoming',
-    geo = dict(
+    # title_text='Covid-19 In the United States and Wyoming',
+    geo=dict(
         scope='usa',
-        projection=go.layout.geo.Projection(type = 'albers usa'),
-        showlakes=False, # lakes
+        projection=go.layout.geo.Projection(type='albers usa'),
+        showlakes=False,  # lakes
         lakecolor='rgb(0, 0, 255)'),
 )
+
+
 def get_fig():
     return fig
+
 
 fig.show()
