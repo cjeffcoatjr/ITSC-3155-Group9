e08c8c1ed0d33e67e227578101705397c49ddb22
cjeffcoatjr <cjeffcoatr@gmail.com>
2020-04-17 19:30:38 -0400

    Formed the basis for a Dashboard

--- final-project/Dashboard/Dashboard.py
+++ final-project/Dashboard/Dashboard.py
@@ -2,11 +2,23 @@ import dash
 from Data import ChoroplethEx as data
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.graph_objs as go
 
 app = dash.Dash()  # instate the dashboard
+interactive_map = data.get_fig()  # From the data, pull a plotly object
 
-# From the data, pull a plotly object
-interactive_map = data.init
-
+# Layout
+app.layout = html.Div(children=[
+    html.H1(children='Interactive Dashboard',
+            style={
+                'textAlign': 'center',
+                'color': '#ef3e18'
+            }
+            ),
+    html.Div('A Python Dashboard for COVID-19 Visualization using Plotly', style={'textAlign': 'center'}),
+    html.Br(),
+    html.Hr(style={'color': '#7FDBFF'}),
+    html.H3('This interactive map visualizes COVID-19 impact across the United States of America and Wyoming.',
+            style={'color': '#df2e56'}),
+    dcc.Graph(figure=interactive_map)
+])
+app.run_server()
