9b91e4bea260240e343710836c79bf35573fe9e3
rook <tcurry10@uncc.edu>
2020-04-17 18:34:40 -0400

    Added more to the example

--- final-project/Data/ChoroplethEx.py
+++ final-project/Data/ChoroplethEx.py
@@ -9,19 +9,36 @@ else:
     print("error, server responded with status code of" + str(response.status_code))
     exit(-1)
 
-# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
-df = pd.read_json(stateDict)
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+# df = pd.read_json(stateDict)
+
+for col in df.columns:
+    df[col] = df[col].astype(str)
+
+df['text'] = df['state'] + '<br>' + \
+    'Beef ' + df['beef'] + ' Dairy ' + df['dairy'] + '<br>' + \
+    'Fruits ' + df['total fruits'] + ' Veggies ' + df['total veggies'] + '<br>' + \
+    'Wheat ' + df['wheat'] + ' Corn ' + df['corn']
+
 fig = go.Figure(data=go.Choropleth(
-    locations=df['code'], # Spatial coordinates
-    z = df['total exports'].astype(float), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = 'Reds',
-    colorbar_title = "Infected",
+    locations=df['code'],
+    z=df['total exports'].astype(float),
+    locationmode='USA-states',
+    colorscale='Reds',
+    autocolorscale=False,
+    text=df['text'], # hover text
+    marker_line_color='white', # line markers between states
+    colorbar_title="Infected"
 ))
 
+
 fig.update_layout(
-    title_text = 'Covid-19 In the United States and Wyoming',
-    geo_scope='usa', # limite map scope to USA
+    title_text='Covid-19 In the United States and Wyoming',
+    geo = dict(
+        scope='usa',
+        projection=go.layout.geo.Projection(type = 'albers usa'),
+        showlakes=True, # lakes
+        lakecolor='rgb(255, 255, 255)'),
 )
 
 fig.show()
