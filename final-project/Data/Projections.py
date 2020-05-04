b21c41f263c77141148011bd2fbbe5593bac2d8a
cjeffcoatjr <cjeffcoatjr@gmail.com>
2020-05-04 19:28:00 -0400

    Proper projections data, for real this time.

--- final-project/Data/Projections.py
+++ final-project/Data/Projections.py
@@ -1,4 +1,5 @@
 import requests
+import numpy as np
 from Data import States as states
 
 response = {}
@@ -7,17 +8,17 @@ for state in states.stateList:
     tmp_response = requests.get(("https://disease.sh/v2/historical/usacounties/" + tmp_state + "?lastdays=30"))
     if tmp_response.status_code == 200:
         tmp_response = tmp_response.json()
-        cases = {}
-        deaths = {}
+        cases = np.zeros(30)
+        deaths = np.zeros(30)
         for county in tmp_response:  # Merge per-county data into per-state data
             tmp_cases = county['timeline']['cases']
-            updated_cases = cases
-            cases = {**tmp_cases, **updated_cases}
+            cases_nparray = np.array(list(county['timeline']['cases'].values()))
+            cases = cases + cases_nparray
             tmp_deaths = county['timeline']['deaths']
-            updated_deaths = deaths
-            deaths = {**tmp_deaths, **updated_deaths}
-        cases = list(cases.values())
-        deaths = list(deaths.values())
+            deaths_nparray = np.array(list(county['timeline']['deaths'].values()))
+            deaths = deaths + deaths_nparray
+        cases = cases.tolist()
+        deaths = deaths.tolist()
         tmp_dict = {'cases': cases, 'deaths': deaths}
         abbrev = states.states[state]
         response[abbrev] = tmp_dict
