c0f643995794b5c7006e56f25b2e5cd3140de5b9
cjeffcoatjr <cjeffcoatjr@gmail.com>
2020-05-05 20:29:51 -0400

    New figs

--- final-project/Data/ChoroplethProjections.py
+++ final-project/Data/ChoroplethProjections.py
@@ -1,5 +1,4 @@
 import plotly.graph_objects as go
-import requests
 import pandas as pd  # Load data frame and tidy it.
 from Data import ProjectionsCalcRework as projections
 
@@ -12,21 +11,50 @@ for col in weekly_df.columns:
 for col in monthly_df.columns:
     monthly_df[col] = monthly_df[col].astype(str)
 
-weekly_df['text'] = 'Total Projected Cases Tomorrow: ' + weekly_df['cases'] + '<br>' + \
-             'Total Deaths: ' + weekly_df['deaths']
+weekly_df['text'] = 'Total Projected Cases Tomorrow: ' + weekly_df['cases'][0] + '<br>' + \
+                    'Total Projected Cases in 14 days: ' + weekly_df['cases'][1] + '<br>' + \
+                    'Total Projected Cases in 30 days: ' + weekly_df['cases'][2] + '<br>' + \
+                    'Total Projected Deaths Tomorrow: ' + weekly_df['deaths'][0] + '<br>' + \
+                    'Total Projected Deaths in 14 days: ' + weekly_df['deaths'][0] + '<br>' + \
+                    'Total Projected Deaths in 30 days: ' + weekly_df['deaths'][0]
 
-fig = go.Figure(data=go.Choropleth(
-    locations=df['state'],
-    z=df['cases'].astype(int),
+monthly_df['text'] = 'Total Projected Cases Tomorrow: ' + monthly_df['cases'][0] + '<br>' + \
+                     'Total Projected Cases in 14 days: ' + monthly_df['cases'][1] + '<br>' + \
+                     'Total Projected Cases in 30 days: ' + monthly_df['cases'][2] + '<br>' + \
+                     'Total Projected Deaths Tomorrow: ' + monthly_df['deaths'][0] + '<br>' + \
+                     'Total Projected Deaths in 14 days: ' + monthly_df['deaths'][0] + '<br>' + \
+                     'Total Projected Deaths in 30 days: ' + monthly_df['deaths'][0]
+
+weekly_fig = go.Figure(data=go.Choropleth(
+    locations=weekly_df['state'],
+    z=weekly_df['cases'][0].astype(int),
     locationmode='USA-states',
     colorscale='Reds',
     autocolorscale=False,
-    text=df['text'],  # hover text
+    text=weekly_df['text'],  # hover text
     marker_line_color='white',  # line markers between states
     colorbar_title="Cases"
 ))
 
-fig.update_layout(
+weekly_df.update_layout(
+    geo=dict(
+        scope='usa',
+        projection=go.layout.geo.Projection(type='albers usa'),
+        showlakes=False),
+)
+
+monthly_fig = go.Figure(data=go.Choropleth(
+    locations=monthly_df['state'],
+    z=monthly_df['cases'][0].astype(int),
+    locationmode='USA-states',
+    colorscale='Reds',
+    autocolorscale=False,
+    text=monthly_df['text'],  # hover text
+    marker_line_color='white',  # line markers between states
+    colorbar_title="Cases"
+))
+
+monthly_df.update_layout(
     geo=dict(
         scope='usa',
         projection=go.layout.geo.Projection(type='albers usa'),
@@ -34,5 +62,9 @@ fig.update_layout(
 )
 
 
-def get_fig():
-    return fig
+def get_weekly_fig():
+    return weekly_df
+
+
+def get_monthly_fig():
+    return monthly_df
